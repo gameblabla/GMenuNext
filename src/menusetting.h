f3207a12c99d5d0ae9ed02b98c438037e9f30585
pingflood <pingflood@gmail.com>
2018-07-10 21:37:54 -0300

    Share btn among all menu subclasses

--- src/menusetting.h
+++ src/menusetting.h
@@ -21,6 +21,7 @@
 #define MENUSETTING_H
 
 #include "buttonbox.h"
+#include "iconbutton.h"
 
 #include <string>
 
@@ -39,6 +40,7 @@ protected:
 
 public:
 	MenuSetting(GMenu2X *gmenu2x, const std::string &title, const std::string &description);
+	IconButton *btn;
 	virtual ~MenuSetting();
 
 	virtual void draw(int y);
