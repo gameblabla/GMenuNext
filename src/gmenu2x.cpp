b4dbcffb9c011070d6d1a07c5fabb7315542287b
pingflood <pingflood@gmail.com>
2018-07-20 18:43:19 -0300

    Turn off LCD on reboot/poweroff

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -1751,7 +1751,7 @@ void GMenu2X::poweroffDialog() {
 		MessageBox mb(this, tr["Poweroff"]);
 		mb.setAutoHide(500);
 		mb.exec();
-
+		setBacklight(0);
 #if !defined(TARGET_PC)
 		system("poweroff");
 #endif
@@ -1760,7 +1760,7 @@ void GMenu2X::poweroffDialog() {
 		MessageBox mb(this, tr["Rebooting"]);
 		mb.setAutoHide(500);
 		mb.exec();
-
+		setBacklight(0);
 #if !defined(TARGET_PC)
 		system("reboot");
 #endif
